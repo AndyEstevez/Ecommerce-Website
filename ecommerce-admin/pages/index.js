a3d0608aff2ad36363507400e39ca4d68976cab7
Andy <andyestevez27@gmail.com>
2023-09-07 15:40:19 -0400

    Added products page, update home page

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -2,12 +2,25 @@
 // import { Inter } from 'next/font/google'
 
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
+  const {data: session} = useSession();
   return <Layout>
-    test
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.email}</b>
+
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
+    </div>
   </Layout>
 }
