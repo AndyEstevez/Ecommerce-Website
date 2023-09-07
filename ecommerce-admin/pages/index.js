1c14308882cf022356515a32fef66ee6aef701ee
Andy <andyestevez27@gmail.com>
2023-09-07 14:46:52 -0400

    Added navbar, layout for each page in layout.js

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,26 +1,13 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 
+import Layout from "@/components/Layout";
+
 // const inter = Inter({ subsets: ['latin'] })
 
-import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if(!session) { // user is not logged in 
-    return(
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>
-    )
-  }
-  //logged in
-  return (
-    <div>logged in {session.user.email}</div>
-  )
-
- 
+  return <Layout>
+    test
+  </Layout>
 }
