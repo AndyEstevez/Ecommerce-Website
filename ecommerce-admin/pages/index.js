fb844db9bba2ead1a443c72ba38981f810d4b5cb
Andy <andyestevez27@gmail.com>
2023-09-07 13:37:59 -0400

    added mongodb connection

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -3,12 +3,24 @@
 
 // const inter = Inter({ subsets: ['latin'] })
 
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  const { data: session } = useSession();
+
+  if(!session) { // user is not logged in 
+    return(
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
       </div>
-    </div>
+    )
+  }
+  //logged in
+  return (
+    <div>logged in {session.user.email}</div>
   )
+
+ 
 }
