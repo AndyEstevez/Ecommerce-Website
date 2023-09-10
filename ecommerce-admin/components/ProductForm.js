269884cb0311e0d201462d94a688e420cb1fbc42
Andy <andyestevez27@gmail.com>
2023-09-10 17:17:07 -0400

    added spinner when uploading image, can sort images, images added to mongodb

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -2,6 +2,8 @@ import Layout from '@/components/Layout'
 import { useState } from 'react'
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import Spinner from './Spinner';
+import { ReactSortable } from 'react-sortablejs';
 
 
 // form to create products, or edit existing products in the database
@@ -10,18 +12,20 @@ export default function ProductForm({
     title:existingTitle, 
     description:existingDescription, 
     price:existingPrice,
-    images,
+    images:existingImages,
 
 }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
+    const [images, setImages] = useState(existingImages || []);
+    const [isUploading, setIsUploading] = useState(false);
     const [goToProducts, setGoToProducts] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev){
         ev.preventDefault();
-        const data = {title, description, price}
+        const data = {title, description, price, images}
 
         if(_id) { // if we have id, then update 
             await axios.put('/api/products', {...data, _id});
@@ -43,16 +47,24 @@ export default function ProductForm({
     async function uploadImages(ev) {
         const files = ev.target?.files;
         if(files?.length > 0){
+            setIsUploading(true);
             const data = new FormData();
             for(const file of files){
                 data.append('file', file);
             }
-            const res = await fetch('/api/upload', {
-                method: 'POST',
-                body: data,
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
             })
-            console.log(res)
+            setIsUploading(false)
+            console.log(res.data)
         }
+    }
+
+    // save order of images, when user sorts images
+    function updateImagesOrder(images){
+        setImages(images)
+
     }
 
     return(
@@ -64,7 +76,22 @@ export default function ProductForm({
                 <label>
                     Photos
                 </label>  
-                <div className='mb-2'> 
+                <div className='mb-2 flex flex-wrap gap-1'> 
+                    <ReactSortable list={images} setList={updateImagesOrder} className='flex flex-wrap gap-1'>
+
+                        {!!images?.length && images.map(link => (
+                            <div key={link} className='h-24'>
+                                <img src={link} alt='' className='rounded-lg'/>
+                            </div>
+                        ))}
+                    </ReactSortable>
+
+                    {isUploading && (
+                        <div className='h-24 flex items-center'>
+                            <Spinner/>
+                        </div>
+                    )}
+                    
                     <label className='w-24 h-24 flex items-center text-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -74,9 +101,7 @@ export default function ProductForm({
                         </div>
                         <input type="file" className='hidden' onChange={uploadImages}/>
                     </label>
-                    {!images?.length && (
-                        <div>No photos in this product</div>
-                    )}
+                    
                 </div>  
 
                 <label>Description</label>
