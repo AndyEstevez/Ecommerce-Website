41750e0032c553abd0d74a297e3d7daa4041216f
Andy <andyestevez27@gmail.com>
2023-09-11 12:22:47 -0400

    Added categories page, categories schema for mongodb, (get, post, put) requests for categories

---
+++ ecommerce-admin/pages/api/categories.js
@@ -0,0 +1,32 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+
+export default async function handle(req, res) {
+    const {method} = req;
+    await mongooseConnect();
+
+
+    // get all categories from mongodb
+    if (method === 'GET'){
+        res.json(await Category.find().populate('parent'));
+    }
+
+    // create category in mongodb
+    if (method === 'POST') {
+        const {name, parentCategory} = req.body;
+        const categoryDoc = await Category.create({name, parent:parentCategory || undefined});
+        res.json(categoryDoc);
+    }
+
+    // update category after saving edit
+    if (method === 'PUT') {
+        const  {name, parentCategory, _id} = req.body;
+        const categoryDoc = await Category.updateOne({_id}, {
+            name,
+            parent: parentCategory
+        });
+        res.json(categoryDoc)
+    }
+
+
+}
