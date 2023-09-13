c83fa56430ac41212b9a8fecec0af23a25e36df8
Andy <andyestevez27@gmail.com>
2023-09-12 20:14:00 -0400

    Added featured product to home page and mongodb connection

--- ecommerce-front/pages/index.js
+++ ecommerce-front/pages/index.js
@@ -1,11 +1,24 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
 
-export default function HomePage() {
+import { Product } from "@/models/Product";
+
+export default function HomePage({product}) {
   return (
     <div>
       <Header />
-      <Featured/>
+      <Featured product={product}/>
     </div>
   )
+}
+
+export async function getServerSideProps(){
+  const featuredProductId = '6500f4a56eb878a3b7be5eb9';
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    // parse the string to an object in JSON
+    props: {product: JSON.parse(JSON.stringify(product))},
+  }
 }
